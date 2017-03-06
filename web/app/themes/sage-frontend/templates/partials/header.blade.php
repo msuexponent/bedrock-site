06802d28fe97a22a710f4b055390596e0f498fd7
Tyler Foulkes <tylerfoulkes10@gmail.com>
2017-03-05 21:42:23 -0700

    Organized mobile nav dropdown

--- web/app/themes/sage-frontend/templates/partials/header.blade.php
+++ web/app/themes/sage-frontend/templates/partials/header.blade.php
@@ -52,10 +52,10 @@
 	<nav class="row-fluid nav navbar-toggleable-sm background-blue content">
 		<div class="collapse navbar-collapse" id="nav-drop">
 			@if (has_nav_menu('primary_navigation'))
-				{!! wp_nav_menu(['container' => false, 'theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav left-nav col-xs-6 col-md-7 float-left']) !!}
+				{!! wp_nav_menu(['container' => false, 'theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav left-nav col-12 col-md-7 float-md-left']) !!}
 			@endif
 			@if (has_nav_menu('top_right_navigation'))
-				{!! wp_nav_menu(['container' => false, 'theme_location' => 'top_right_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav right-nav float-right col-4 col-xl-3']) !!}
+				{!! wp_nav_menu(['container' => false, 'theme_location' => 'top_right_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav right-nav float-md-right col-12 col-md-4 col-xl-3']) !!}
 			@endif
 		</div>
 	</nav>
