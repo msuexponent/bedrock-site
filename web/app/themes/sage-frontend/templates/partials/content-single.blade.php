de58e9ee3a7383ab0bd6835476562c155ee4a613
Tyler Foulkes <tylerfoulkes10@gmail.com>
2017-03-14 21:48:20 -0600

    Refined post gallery structure

--- web/app/themes/sage-frontend/templates/partials/content-single.blade.php
+++ web/app/themes/sage-frontend/templates/partials/content-single.blade.php
@@ -1,25 +1,31 @@
 <article @php(post_class())>
-  	<header>
-		<h1 class="entry-title">{{ get_the_title() }}</h1>
-		@include('partials/entry-meta')
-  	</header>
+	<h1 class="entry-title">{{ get_the_title() }}</h1>
+	@include('partials/entry-meta')
   	<div class="entry-content">
-		@php(the_content())
+		@php(the_excerpt())
 
 		@if (get_post_gallery())
-        	@php($gallery = get_post_gallery( get_the_ID(), false ))
-	        <div id="media-carousel" class="carousel slide top10 bottom40" data-ride="carousel">
-	        	<div class="carousel-inner" role="listbox">
-		            @foreach( $gallery['src'] as $src ) : ?>
-		                <div class="carousel-item">
-    						<img class="img-fluid" src="{!! $src !!}"/>
-    					</div>
-		            @endforeach
-				  	<a class="left carousel-control" href="#media-carousel" role="button" data-slide="prev">
+			<div class="col-12">
+	        	@php($gallery = get_post_gallery( get_the_ID(), false ))
+		        <div id="gallery-carousel" class="carousel slide top10 bottom40" data-ride="carousel">
+		        	<ol class="carousel-indicators">
+		        		@php($i = 0)
+		        		@foreach( $gallery['src'] as $src )
+					    	<li data-target="#media-carousel" data-slide-to="@php($i++)" class="active"></li>
+					    @endforeach
+				  	</ol>
+		        	<div class="carousel-inner active" role="listbox">
+			            @foreach( $gallery['src'] as $src )
+			                <div class="carousel-item active">
+	    						 <img class="d-block img-fluid" src="{!! $src !!}" alt="First slide" width="640" height="360"/>
+	    					</div>
+			            @endforeach
+			        </div>
+				  	<a class="left carousel-control" href="#gallery-carousel" role="button" data-slide="prev">
 				    	<span class="icon-prev" aria-hidden="true"></span>
 				    	<span class="sr-only">Previous</span>
 				  	</a>
-				  	<a class="right carousel-control" href="#media-carousel" role="button" data-slide="next">
+				  	<a class="right carousel-control" href="#gallery-carousel" role="button" data-slide="next">
 				    	<span class="icon-next" aria-hidden="true"></span>
 				    	<span class="sr-only">Next</span>
 					</a>
@@ -27,8 +33,6 @@
 			</div>
     	@endif
   	</div>
-  	<footer>
-		{!! wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  	</footer>
+
   	@php(comments_template('/templates/partials/comments.blade.php'))
 </article>
