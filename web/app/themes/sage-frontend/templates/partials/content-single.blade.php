743f8192f62586a04f104ced02e4bf461608384d
Tyler Foulkes <tylerfoulkes10@gmail.com>
2017-04-16 22:46:47 -0600

    Added simple share buttons shortcode

--- web/app/themes/sage-frontend/templates/partials/content-single.blade.php
+++ web/app/themes/sage-frontend/templates/partials/content-single.blade.php
@@ -5,11 +5,14 @@
 				<h1 class="entry-title">{{ get_the_title() }}</h1>
 				@include('partials/entry-meta')
 			</div>
-
+			<p>
+			test
 			<?php
-    $gallery_shortcode = '[gallery id="' . intval( $post ) . '"]';
-    print apply_filters( 'the_content', $gallery_shortcode );
+    // $gallery_shortcode = '[gallery id="' . intval( $post->postparent ) . '"]';
+    // print apply_filters( 'the_content', $gallery_shortcode );
+			//do_shortcode('[slick-carousel]');
  ?>
+ </p>
 
 			@if ($gallery = get_post_gallery( get_the_ID(), false ))
 				
@@ -50,6 +53,7 @@
 			</div>
 
 	    	<div class="col-12 top20">
+	    		{!! do_shortcode('[ssba]') !!}
 		    	@php($categories = get_the_category())
 		 		@php($tags = get_the_tags())
 
