7428891491dd5739ae512df8939bfe0cabece8fa
Tyler Foulkes <tylerfoulkes10@gmail.com>
2017-07-23 19:42:34 -0600

    Styled navigation but functionality is still not there

--- web/app/themes/sage-frontend/templates/404.blade.php
+++ web/app/themes/sage-frontend/templates/404.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.base')
 
 @section('content')
-  @include('partials.page-header')
+  	<div class="content pad-top40 pad-bottom40">
+  		<div class="col-12">
+			@if (!have_posts())
+				<div class="alert alert-warning">
+  					{{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+				</div>
+				{!! get_search_form(false) !!}
+			@endif
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  {!! get_the_posts_navigation() !!}
+			{!! get_the_posts_navigation() !!}
+  		</div>
+  	</div>
 @endsection
